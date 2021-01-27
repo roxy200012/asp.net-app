276bed9a991b4cbe0f3f828db1cf3c7725e1c3b8
roxy200012 <70378880+roxy200012@users.noreply.github.com>
2021-01-27 11:33:56 +0100

    my first asp.net app

---
+++ RazorPagesMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Modelli;
+
+namespace RazorPagesMovie.Data
+{
+    public class RazorPagesMovieContext : DbContext
+    {
+        public RazorPagesMovieContext (DbContextOptions<RazorPagesMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie.Modelli.Movie> Movie { get; set; }
+    }
+}
